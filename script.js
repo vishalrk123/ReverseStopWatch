9fa55fd256255676daca670f8de54d8caa58185d
vishalrk123 <vishalk13000121@gmail.com>
2026-05-31 00:15:45 +0530

    new change in code

--- script.js
+++ script.js
@@ -16,15 +16,24 @@ let msec = 0;
 let interval = null;
 let flag = true;
 
-// Alarm sound
+// Alarm Sound
 let audio = new Audio(
   "https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg",
 );
 
+// Timer Function
 function timer() {
   // stop at exact zero
-  if (min === 0 && sec === 0 && msec === 0) {
+  if (min === 0 && sec === 0 && msec <= 0) {
     clearInterval(interval);
+
+    min = 0;
+    sec = 0;
+    msec = 0;
+
+    minEle.textContent = "00 : ";
+    secEle.textContent = "00 : ";
+    msecEle.textContent = "000";
 
     audio.play();
 
@@ -55,10 +64,12 @@ function timer() {
   minEle.textContent = cancatZero(min) + " : ";
 }
 
+// Add Zero
 function cancatZero(time) {
   return time <= 9 ? "0" + time : time;
 }
 
+// Format Milliseconds
 function formatMsec(time) {
   if (time <= 9) {
     return "00" + time;
@@ -72,11 +83,12 @@ function formatMsec(time) {
 // START
 startBtn.addEventListener("click", function () {
   if (flag) {
-    // fresh input set
+    // set fresh time
     if (min === 0 && sec === 0 && msec === 0) {
       min = Number(setMin.value) || 0;
       sec = Number(setSec.value) || 0;
-      msec = 0;
+
+      msec = 990;
     }
 
     // validation
